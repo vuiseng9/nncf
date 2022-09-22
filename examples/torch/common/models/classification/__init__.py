324cad21a2079bcdd2f40e028e37a192ef34a6d8
Vui Seng Chua <vui.seng.chua@intel.com>
2022-09-22 08:27:57 -0700

    Disable efficient net import

--- examples/torch/common/models/classification/__init__.py
+++ examples/torch/common/models/classification/__init__.py
@@ -13,7 +13,7 @@
 from .inceptionv3_cifar100 import *
 from .resnet_cifar import *
 from .rmnet_cifar import *
-from .efficientnet import *
+# from .efficientnet import *
 from .mobilenet_v2_cifar10 import *
 from .resnet_cifar10 import *
 from .vgg11_bn_cifar10 import *
