704fe4e53768e7ba3fa5da474609e57c5d22f3dc
yujiepan <yujie.pan@intel.com>
2022-10-19 15:35:45 +0800

    test for `binary_mask_by_threshold`

--- tests/torch/sparsity/movement/test_components.py
+++ tests/torch/sparsity/movement/test_components.py
@@ -3,6 +3,7 @@ import pytest
 import torch
 from nncf.torch import create_compressed_model
 from nncf.torch.sparsity.movement.algo import StructuredMask
+from nncf.torch.sparsity.movement.functions import binary_mask_by_threshold
 from tests.torch.sparsity.movement.helpers import (ConfigBuilder,
                                                    bert_tiny_unpretrained)
 
@@ -26,3 +27,18 @@ def test_structured_mask_setter(tmp_path):
     assert torch.allclose(ctx.dependent_structured_mask, ref_mask)
     with pytest.raises(ValueError):
         ctx.dependent_structured_mask = ref_mask[:1, :1]
+
+
+@pytest.mark.parametrize(("input_tensor", "threshold", "max_percentile", "ref_output_tensor"), [
+    (torch.tensor([1., 2, 3, 4]), 0., 0.9, torch.tensor([1., 1, 1, 1])),
+    (torch.tensor([1., 2, 3, 4]), 2.5, 0.8, torch.tensor([0., 0, 1, 1])),
+    (torch.tensor([1., 2, 3, 4]), 2.5, 0.2, torch.tensor([0., 1, 1, 1])),
+    (torch.tensor([1., 2, 3, 4]), 5., 0.8, torch.tensor([0., 0, 0, 1])),
+    (torch.tensor([1., 1, 1, 1]), 5., 0.8, torch.tensor([0., 0, 0, 0])),
+])
+def test_binary_mask_by_threshold(input_tensor, threshold, max_percentile, ref_output_tensor):
+    for requires_grad in [True, False]:
+        input_tensor.requires_grad_(requires_grad)
+        output_tensor = binary_mask_by_threshold(input_tensor, threshold, max_percentile)
+        assert torch.allclose(output_tensor, ref_output_tensor)
+        assert output_tensor.requires_grad is requires_grad
