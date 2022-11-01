f31868622899d9546e245736124beb12e1cf529e
Yujie Pan <105195712+yujiepan-work@users.noreply.github.com>
2022-11-02 04:24:56 +0800

    Code refactor for movement sparsity, modularizing structured masking (#14)
    * initial draft for structured mask refactor
    
    * use Registry and StructuredMaskRule for strategy
    
    * change way of stuctured mask calculation
    
    * bugfix for structured mask device
    
    * change logic to set stuctured mask
    
    * change compression_stage
    
    * internal change for structured mask
    
    * refactor scheduler
    
    * refactor layers
    
    * bugfix for scheduler when update_per_optimizer_step=False
    
    * reafctor for layer loss & forward
    
    * simplify layer.py
    
    * add __hash__ func for BuildingBlockType
    
    * revise tests due to refactor

--- nncf/experimental/torch/search_building_blocks/search_blocks.py
+++ nncf/experimental/torch/search_building_blocks/search_blocks.py
@@ -127,6 +127,9 @@ class BuildingBlockType(Enum):
 
     def __eq__(self, other: 'BuildingBlockType'):
         return self.__dict__ == other.__dict__
+    
+    def __hash__(self) -> int:
+        return hash((self.__class__.__name__, self.name))
 
 
 class EBBlocksStateNames:
