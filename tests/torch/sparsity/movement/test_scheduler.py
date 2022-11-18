dc36c4fb0611faabc20fa8346127c136e6197354
yujiepan <yujie.pan@intel.com>
2022-11-18 13:53:07 +0800

    beautify test scheduler parameters

--- tests/torch/sparsity/movement/test_scheduler.py
+++ tests/torch/sparsity/movement/test_scheduler.py
@@ -5,51 +5,58 @@ import nncf
 import numpy as np
 import pytest
 from nncf.common.sparsity.schedulers import PolynomialThresholdScheduler
+from tests.torch.sparsity.movement.helpers import SchedulerParams
 from pytest import approx
 
-# TODO: add enable_structured_masking test
-
-class SchedulerParams:
-    def __init__(self, power: int = 3,
-                 warmup_start_epoch: int = 1,
-                 warmup_end_epoch: int = 3,
-                 init_importance_threshold: float = -1.0,
-                 final_importance_threshold: float = 0.0,
-                 importance_regularization_factor: float = 0.1,
-                 steps_per_epoch: Optional[int] = 4,
-                 enable_structured_masking: bool = True):
-        self.power = power
-        self.warmup_start_epoch = warmup_start_epoch
-        self.warmup_end_epoch = warmup_end_epoch
-        self.init_importance_threshold = init_importance_threshold
-        self.final_importance_threshold = final_importance_threshold
-        self.importance_regularization_factor = importance_regularization_factor
-        self.steps_per_epoch = steps_per_epoch
-        self.enable_structured_masking = enable_structured_masking
+# TODO(yujie): add enable_structured_masking test
 
 
-@pytest.mark.parametrize('params,ref_threshold,ref_factor', [
-    (SchedulerParams(2, 1, 3, -1, 0, 0.1, 4),  # normal use
-     [-1., -1., -1., -1., -1., -0.7656, -0.5625, -0.3906, -0.2500, -0.1406, -0.0625, -0.0156, 0., 0., 0., 0., 0., 0., 0., 0.],
-     [0., 0., 0., 0., 0., 0.0234, 0.0438, 0.0609, 0.0750, 0.0859, 0.0938, 0.0984, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]),
-    (SchedulerParams(4, 2, 8, 0, 5, 10, 3),  # warm up range overflow
-     [0., 0., 0., 0., 0., 0., 0., 1.0219, 1.8785, 2.5887, 3.1702, 3.6396, 4.0123, 4.3027, 4.5237],
-     [0., 0., 0., 0., 0., 0., 0., 2.0438, 3.7570, 5.1775, 6.3405, 7.2793, 8.0247, 8.6053, 9.0474]),
-    (SchedulerParams(4, -1, 0, -1, 2, 5, 2),  # no warmup
-     [2.] * 10,
-     [5.] * 10),
-])
-def test_scheduler_can_produce_decayed_importance_threshold_and_regularization_factor(params: SchedulerParams, ref_threshold, ref_factor):
-    scheduler = PolynomialThresholdScheduler(controller=MagicMock(), params=params.__dict__)
+desc_test_decayed_importance_threshold_and_regularization_factor = {
+    "normal_warmup_range": dict(
+        params=SchedulerParams(power=2,
+                               warmup_start_epoch=1, warmup_end_epoch=3,
+                               init_importance_threshold=-1, final_importance_threshold=0,
+                               importance_regularization_factor=0.1, steps_per_epoch=4,
+                               enable_structured_masking=False),
+        ref_threshold=[-1., -1., -1., -1., -1., -0.7656, -0.5625, -0.3906, -0.2500, -0.1406,
+                       -0.0625, -0.0156, 0., 0., 0., 0., 0., 0., 0., 0.],
+        ref_factor=[0., 0., 0., 0., 0., 0.0234, 0.0438, 0.0609, 0.0750, 0.0859, 0.0938, 0.0984,
+                    0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+    ),
+    "overflowed_warmup_range": dict(
+        params=SchedulerParams(power=4,
+                               warmup_start_epoch=2, warmup_end_epoch=8,
+                               init_importance_threshold=0, final_importance_threshold=5,
+                               importance_regularization_factor=10, steps_per_epoch=3,
+                               enable_structured_masking=False),
+        ref_threshold=[0., 0., 0., 0., 0., 0., 0., 1.0219, 1.8785, 2.5887, 3.1702, 3.6396, 4.0123, 4.3027, 4.5237],
+        ref_factor=[0., 0., 0., 0., 0., 0., 0., 2.0438, 3.7570, 5.1775, 6.3405, 7.2793, 8.0247, 8.6053, 9.0474]
+    ),
+    "no_warmup": dict(
+        params=SchedulerParams(power=4,
+                               warmup_start_epoch=-1, warmup_end_epoch=0,
+                               init_importance_threshold=-1, final_importance_threshold=2,
+                               importance_regularization_factor=5, steps_per_epoch=2,
+                               enable_structured_masking=True),
+        ref_threshold=[2.] * 10,
+        ref_factor=[5.] * 10),
+}
+
+
+@pytest.mark.parametrize('desc',
+                         desc_test_decayed_importance_threshold_and_regularization_factor.values(),
+                         ids=desc_test_decayed_importance_threshold_and_regularization_factor.keys())
+def test_scheduler_decayed_importance_threshold_and_regularization_factor(desc):
+    scheduler = PolynomialThresholdScheduler(controller=MagicMock(), params=desc['params'].__dict__)
     threshold, factor = [], []
     for epoch in range(5):
         scheduler.epoch_step()
-        for batch in range(params.steps_per_epoch):
+        for batch in range(desc['params'].steps_per_epoch):
             scheduler.step()
             threshold.append(scheduler.current_importance_threshold)
             factor.append(scheduler.current_importance_lambda)
-    assert np.allclose(threshold, ref_threshold, atol=1e-4)
-    assert np.allclose(factor, ref_factor, atol=1e-4)
+    assert np.allclose(threshold, desc['ref_threshold'], atol=1e-4)
+    assert np.allclose(factor, desc['ref_factor'], atol=1e-4)
 
 
 def test_scheduler_get_state():
@@ -140,5 +147,5 @@ def test_scheduler_can_infer_steps_per_epoch():
 
 def test_scheduler_raises_error_of_improper_steps_per_epoch_setting():
     params = SchedulerParams(warmup_start_epoch=0, steps_per_epoch=None)
-    with pytest.raises(ValueError):    
+    with pytest.raises(ValueError):
         _ = PolynomialThresholdScheduler(controller=MagicMock(), params=params.__dict__)
