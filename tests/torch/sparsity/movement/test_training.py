cf8665406886c88131f102a7f826589ac30a1955
yujiepan <yujie.pan@intel.com>
2022-11-21 02:31:35 +0800

    mrpc run config update

--- tests/torch/sparsity/movement/test_training.py
+++ tests/torch/sparsity/movement/test_training.py
@@ -219,8 +219,8 @@ mrpc_movement_desc_template = \
     .real_dataset("mrpc")\
     .config_name("bert_tiny_uncased_mrpc_movement.json")\
     .learning_rate(1e-4)\
-    .batch_size(128)\
-    .num_train_epochs(5)\
+    .batch_size(64)\
+    .num_train_epochs(6)\
     .seed(42)\
 
 MOVEMENT_DESCRIPTORS = {
