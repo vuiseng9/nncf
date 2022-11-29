25503c87fd3fed6dc83b47d25c7097ff43d18871
yujiepan <yujie.pan@intel.com>
2022-11-29 16:48:04 +0800

    copyright

--- nncf/experimental/torch/sparsity/movement/functions.py
+++ nncf/experimental/torch/sparsity/movement/functions.py
@@ -1,5 +1,5 @@
 """
- Copyright (c) 2019 Intel Corporation
+ Copyright (c) 2022 Intel Corporation
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
@@ -10,7 +10,6 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 """
-
 import torch
 
 from nncf.torch.dynamic_graph.patch_pytorch import register_operator
--- nncf/experimental/torch/sparsity/movement/scheduler.py
+++ nncf/experimental/torch/sparsity/movement/scheduler.py
@@ -10,7 +10,6 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 """
-
 from typing import Any, Dict, Optional
 
 from nncf.api.compression import CompressionAlgorithmController
