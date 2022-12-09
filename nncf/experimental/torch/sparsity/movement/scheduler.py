504cd37a87a93358045859cbd2788aa590c83f91
yujiepan <yujie.pan@intel.com>
2022-12-09 11:07:19 +0800

    minor changes

--- nncf/experimental/torch/sparsity/movement/scheduler.py
+++ nncf/experimental/torch/sparsity/movement/scheduler.py
@@ -17,10 +17,10 @@ from typing import Any, Dict, Optional
 import numpy as np
 import torch
 
-from nncf.api.compression import CompressionAlgorithmController
 from nncf.common.schedulers import BaseCompressionScheduler
 from nncf.common.schedulers import PolynomialDecaySchedule
 from nncf.common.utils.logger import logger
+from nncf.torch.sparsity.base_algo import BaseSparsityAlgoController
 
 
 class MovementSchedulerStage(Enum):
@@ -41,7 +41,7 @@ class MovementPolynomialThresholdScheduler(BaseCompressionScheduler):
     scheduler will start calculation only after `steps_per_epoch` is calculated.
     """
 
-    def __init__(self, controller: CompressionAlgorithmController, params: dict):
+    def __init__(self, controller: BaseSparsityAlgoController, params: dict):
         """
         TODO: revise docstring
         Initializes a sparsity scheduler with a polynomial decay schedule.
